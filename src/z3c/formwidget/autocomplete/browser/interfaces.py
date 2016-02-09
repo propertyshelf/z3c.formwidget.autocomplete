83a6d1c8670184e4dfdfb54a7d0b27e59b835853
Zach Cashero <zach@propertyshelf.com>
2016-02-09 16:50:50 -0500

    Simplify the interfaces

--- src/z3c/formwidget/autocomplete/browser/interfaces.py
+++ src/z3c/formwidget/autocomplete/browser/interfaces.py
@@ -2,24 +2,13 @@
 """Interface definitions for z3c.formwidget.autocomplete."""
 
 # zope imports
-from zope.interface import Interface
-from z3c.form.interfaces import IRadioWidget, ICheckBoxWidget
+from z3c.form.interfaces import IRadioWidget
 
 
-class IAutocompleteWidget(Interface):
+class IAutocompleteWidget(IRadioWidget):
     """Marker interface for the autocomplete widget
     """
 
     def bound_source():
-        """The bound QuerySource vocabulary as defined in
-        z3c.formwidget.query"""
-
-
-class IAutocompleteSelectionWidget(IAutocompleteWidget, IRadioWidget):
-    """Interface for the autocomplete selection widget based of the radio
-    widget."""
-
-
-class IAutocompleteMultiSelectionWidget(IAutocompleteWidget, ICheckBoxWidget):
-    """Interface for the autocomplete multi selection widget based of the
-    checkbox widget."""
+        """The bound QuerySource vocabulary as defined in z3c.formwidget.query
+        """
