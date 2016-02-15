2fee3034cb3817534f8dc5a975bf5dd7b3bf9d84
Zach Cashero <zach@propertyshelf.com>
2016-02-15 18:15:44 -0500

    Moved custom implementation for LocationAutocompleteWidget into propertyshelf package

--- src/z3c/formwidget/autocomplete/__init__.py
+++ src/z3c/formwidget/autocomplete/__init__.py
@@ -1,8 +1,5 @@
 """An AJAX autocomplete widget for z3c.form."""
 
 from z3c.formwidget.autocomplete.browser.widget import AutocompleteFieldWidget
-from z3c.formwidget.autocomplete.browser.location import (
-    LocationAutocompleteFieldWidget)
 
 assert(AutocompleteFieldWidget)
-assert(LocationAutocompleteFieldWidget)
